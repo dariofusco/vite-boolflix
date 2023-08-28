82f788888a302f97da7451ad011cf0b08543b2cb
dariofusco <fdario@hotmail.it>
2023-08-28 13:48:42 +0200

    inserisce navbar

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -3,9 +3,20 @@ export default {}
 </script>
 
 <template>
-<header></header>
+    <header>
+        <nav class="navbar navbar-light bg-black px-3">
+            <a class="navbar-brand">Boolflix</a>
+            <form class="form-inline d-flex">
+                <input class="form-control bg-light mx-2" type="cerca" placeholder="Cerca" aria-label="Cerca">
+                <button class="btn btn-outline-danger mx-2" type="submit">Cerca</button>
+            </form>
+        </nav>
+    </header>
 </template>
 
 <style lang="scss" scoped>
-
+.navbar-brand {
+    color: red;
+    font-size: 2rem;
+}
 </style>
