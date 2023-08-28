9c139c90a7d037e297e340558f675341fb23d693
dariofusco <fdario@hotmail.it>
2023-08-28 15:53:53 +0200

    inserisce richiesta axios

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,5 +1,32 @@
 <script>
-export default {}
+import axios from "axios";
+export default {
+    data() {
+        return {
+            filmToSearch: "",
+            filmList: [],
+        };
+    },
+    methods: {
+        searchFilm() {
+            axios
+                .get("https://api.themoviedb.org/3/search/movie", {
+                    params: {
+                        api_key: "0622583111517058bf60e98d473372de",
+                        query: this.filmToSearch,
+                    }
+                })
+                .then((response) => {
+                    this.filmList.push(response.data.results);
+                    console.log(response.data.results);
+                });
+        },
+    },
+    mounted() {
+
+    },
+
+};
 </script>
 
 <template>
@@ -7,8 +34,9 @@ export default {}
         <nav class="navbar navbar-light bg-black px-3">
             <a class="navbar-brand">Boolflix</a>
             <form class="form-inline d-flex">
-                <input class="form-control bg-light mx-2" type="cerca" placeholder="Cerca" aria-label="Cerca">
-                <button class="btn btn-outline-danger mx-2" type="submit">Cerca</button>
+                <input class="form-control bg-light mx-2" v-model="filmToSearch" type="cerca" placeholder="Cerca"
+                    aria-label="Cerca" name="cerca">
+                <button class="btn btn-outline-danger mx-2" @click="searchFilm">Cerca</button>
             </form>
         </nav>
     </header>
