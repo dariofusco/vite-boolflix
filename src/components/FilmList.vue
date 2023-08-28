7ac1b1c66a9cca322ae04cb6874c508267515138
dariofusco <fdario@hotmail.it>
2023-08-28 17:48:21 +0200

    stampa risultati ricerca

--- src/components/FilmList.vue
+++ src/components/FilmList.vue
@@ -14,12 +14,16 @@ export default {
 
 <template>
     <div class="container">
-        <ul v-for="film in store.filmList">
-            <li> Title: {{ film.title }}</li>
-            <li> Original Title: {{ film.original_title }}</li>
-            <li> Language: {{ film.original_language }}</li>
-            <li> Vote: {{ film.vote_average }}</li>
-        </ul>
+        <div class="row">
+            <div class="col-3" v-for="film in store.filmList">
+                <ul>
+                    <li> Title: {{ film.title }}</li>
+                    <li> Original Title: {{ film.original_title }}</li>
+                    <li> Language: {{ film.original_language }}</li>
+                    <li> Vote: {{ film.vote_average }}</li>
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
 
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,29 +1,13 @@
 <script>
-//import axios from "axios";
-import { store, searchFilm} from "../store"
+import { store, searchFilm } from "../store"
 export default {
     data() {
         return {
-            //filmToSearch: "",
-            //filmList: [],
             store
         };
     },
     methods: {
         searchFilm
-        /*searchFilm() {
-            axios
-                .get("https://api.themoviedb.org/3/search/movie", {
-                    params: {
-                        api_key: "0622583111517058bf60e98d473372de",
-                        query: this.filmToSearch,
-                    }
-                })
-                .then((response) => {
-                    this.filmList = response.data.results;
-                    console.log(response.data.results);
-                });
-        },*/
     },
     mounted() {
 
@@ -37,8 +21,8 @@ export default {
         <nav class="navbar navbar-light bg-black px-3">
             <a class="navbar-brand">Boolflix</a>
             <div class="d-flex">
-                <input class="bg-light mx-2" v-model="store.filmToSearch" type="cerca" placeholder="Cerca" aria-label="Cerca"
-                    name="cerca">
+                <input class="bg-light mx-2" v-model="store.filmToSearch" type="cerca" placeholder="Cerca"
+                    aria-label="Cerca" name="cerca">
                 <button class="btn btn-outline-danger mx-2" @click="searchFilm()">Cerca</button>
             </div>
         </nav>
